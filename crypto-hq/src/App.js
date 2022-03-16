fd95f72bf146cc51d50f717ef26bfb51ac1c3b1d
unknown <sshann30@gmail.com>
2022-03-16 14:32:13 -0400

    error with headerS tag fixed

--- crypto-hq/src/App.js
+++ crypto-hq/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">
-      poop
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" component={Homepage} />
+          <Route path="/coins/:id" component={CoinPage} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
